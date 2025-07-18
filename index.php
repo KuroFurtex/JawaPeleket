ad38e2b48f076534f1cc4c6a53fc7a8f14c80e7d
KuroFurtex <79640910+KuroFurtex@users.noreply.github.com>
2025-07-18 08:38:00 +0700

    Sidebar + Navbar

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <html>
 	<head>
 		<title>JawaPeleker</title>
+		<link rel="stylesheet" href="view/css/bootstrap.min.css">
 		<link rel="stylesheet" href="view/core.css">
+		
+		<script src="view/js/bootstrap.min.js"></script>
 	</head>
 	
     <body>
-        <h1>Hanya preview</h1>
+		<?php
+			include 'view/sidebar.php';
+			include 'view/navbar.php'
+		?>
+		<div class="container">
+			<h1>JawaPeleker</h1>
+		</div>
     </body>
 </html>
